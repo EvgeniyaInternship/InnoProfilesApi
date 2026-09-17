d8bd35ec37db39212af2f1317c09b7bbabb58bf6
LastJayG <lastjaygames@gmail.com>
2026-09-17 09:22:58 +0300

    add to Infrastructure: DbContext and Configurations

---
+++ ProfilesApi/ProfilesApi.Infrastructure/Configurations/ReceptionistConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProfilesApi.Domain.Entities;
+
+namespace ProfilesApi.Infrastructure.Configurations;
+
+public class ReceptionistConfiguration : IEntityTypeConfiguration<ReceptionistEntity>
+{
+    public void Configure(EntityTypeBuilder<ReceptionistEntity> builder)
+    {
+        builder.ConfigureBaseEntity();
+        builder.ConfigureSoftDelete();
+        builder.ConfigurePerson();
+
+        builder.Property(x => x.WorkStartDate).IsRequired();
+        builder.Property(x => x.OfficeId).IsRequired();
+        builder.HasIndex(x => x.OfficeId);
+    }
+}
