6b7dfb314be10686f983c9ec88626863555d6687
E. Malinovskaya <146539704+LastJayG@users.noreply.github.com>
2026-09-23 11:12:31 +0300

    EVG-12: Add UnitOfWork (#7)
    * added: UnitOfWork implementation
    
    * refactor: BeginTransactionAsync
    
    * delete: unused property

---
+++ ProfilesApi/ProfilesApi.Infrastructure/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using System.Data;
+
+namespace ProfilesApi.Infrastructure.Interfaces;
+
+public interface IUnitOfWork
+{
+    IAccountRepository Accounts { get; }
+    IDoctorRepository Doctors { get; }
+    IPatientRepository Patients { get; }
+    IReceptionistRepository Receptionists { get; }
+    IAdminRepository Admins { get; }
+
+    int SaveChanges();
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+
+    Task<IDbContextTransaction> BeginTransactionAsync(
+        IsolationLevel isolationLevel = IsolationLevel.ReadCommitted,
+        CancellationToken cancellationToken = default);
+}
---
+++ ProfilesApi/ProfilesApi.Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using ProfilesApi.Infrastructure.Context;
+using ProfilesApi.Infrastructure.Interfaces;
+using System.Data;
+
+namespace ProfilesApi.Infrastructure.Repositories;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly ProfilesDbContext _context;
+
+    public IAccountRepository Accounts { get; }
+    public IDoctorRepository Doctors { get; }
+    public IPatientRepository Patients { get; }
+    public IReceptionistRepository Receptionists { get; }
+    public IAdminRepository Admins { get; }
+
+    public UnitOfWork(
+        ProfilesDbContext context,
+        IAccountRepository accounts,
+        IAdminRepository admins,
+        IDoctorRepository doctors,
+        IReceptionistRepository receptionists,
+        IPatientRepository patients)
+    {
+        _context = context;
+        Accounts = accounts;
+        Admins = admins;
+        Doctors = doctors;
+        Patients = patients;
+        Receptionists = receptionists;
+    }
+
+    public int SaveChanges()
+    {
+        return _context.SaveChanges();
+    }
+
+    public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        return await _context.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task<IDbContextTransaction> BeginTransactionAsync(
+        IsolationLevel isolationLevel = IsolationLevel.ReadCommitted,
+        CancellationToken cancellationToken = default)
+    {
+        return await _context.Database.BeginTransactionAsync(isolationLevel, cancellationToken);
+    }
+}
