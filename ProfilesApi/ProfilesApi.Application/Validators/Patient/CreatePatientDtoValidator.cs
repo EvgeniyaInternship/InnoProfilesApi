440dd51b53e6875f6db8b9af2c9e02693ed6be37
LastJayG <lastjaygames@gmail.com>
2026-09-25 13:19:14 +0300

    add: validators

---
+++ ProfilesApi/ProfilesApi.Application/Validators/Patient/CreatePatientDtoValidator.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using ProfilesApi.Application.DTOs.Requests.CreateRequests;
+
+namespace ProfilesApi.Application.Validators.Patient;
+
+public class CreateReceptionistDtoValidator : AbstractValidator<CreatePatientDto>
+{
+    public CreateReceptionistDtoValidator()
+    {
+        RuleFor(x => x.AccountId)
+            .NotEmpty();
+        RuleFor(x => x.BirthDate)
+            .NotEmpty()
+            .LessThan(DateTime.Now);
+        RuleFor(x => x.FirstName)
+            .NotEmpty()
+            .MaximumLength(50)
+            .WithMessage("First name cannot exceed 50 characters.");
+        RuleFor(x => x.LastName)
+            .NotEmpty()
+            .MaximumLength(50)
+            .WithMessage("Last name cannot exceed 50 characters.");
+        RuleFor(x => x.MiddleName)
+            .MaximumLength(50)
+            .WithMessage("Middle name cannot exceed 50 characters.");
+        RuleFor(x => x.InsuranceNumber)
+            .NotEmpty()
+            .WithMessage("Insurance number is required.")
+            .Matches(@"^(?i)(?!(?:BG|GB|KN|NK|NT|TN|ZZ))[A-CEGHJ-PR-TW-Z][A-NP-Z](?:\s*\d{2}){3}\s*[A-D]$")
+            .WithMessage("Invalid National Insurance Number format.");
+    }
+}
---
+++ ProfilesApi/ProfilesApi.Application/Validators/Receptionist/CreateReceptionistDtoValidator.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation;
+using ProfilesApi.Application.DTOs.Requests.CreateRequests;
+
+namespace ProfilesApi.Application.Validators.Receptionist;
+
+public class CreateReceptionistDtoValidator : AbstractValidator<CreateReceptionistDto>
+{
+    public CreateReceptionistDtoValidator()
+    {
+        RuleFor(x => x.AccountId)
+            .NotEmpty();
+        RuleFor(x => x.BirthDate)
+            .NotEmpty()
+            .LessThan(DateTime.Now);
+        RuleFor(x => x.FirstName)
+            .NotEmpty()
+            .MaximumLength(50)
+            .WithMessage("First name cannot exceed 50 characters.");
+        RuleFor(x => x.LastName)
+            .NotEmpty()
+            .MaximumLength(50)
+            .WithMessage("Last name cannot exceed 50 characters.");
+        RuleFor(x => x.MiddleName)
+            .MaximumLength(50)
+            .WithMessage("Middle name cannot exceed 50 characters.");
+        RuleFor(x => x.WorkStartDate)
+            .NotEmpty()
+            .LessThan(DateTime.Now)
+            .WithMessage("Work start date must be in the past.");
+        RuleFor(x => x.OfficeId)
+            .NotEmpty();
+    }
+}
